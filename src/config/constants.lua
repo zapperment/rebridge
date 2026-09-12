4cadc9baa5bb76c1fdace9828dcb99bbcfdee93c
Patrick Hund <wiekatz@gmail.com>
2026-09-12 06:47:28 +0000

    chore: set software version

--- src/config/constants.lua
+++ src/config/constants.lua
@@ -1,5 +1,5 @@
 return {
-  softwareVersion = "0.0.4 SNAPSHOT",
+  softwareVersion = "0.0.4 BETA",
   -- the number of encoders, faders and buttons on the remote surface
   counts = {
     encoders = 24,
